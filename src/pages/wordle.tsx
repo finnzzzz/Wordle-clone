4152e2d887911e169f662541a55fed6811d29b34
finnzzzz <o8786791@gmail.com>
2023-07-29 19:00:07 +0800

    fix: fix bug occurring after the game end

--- src/pages/wordle.tsx
+++ src/pages/wordle.tsx
@@ -1,8 +1,9 @@
-import "tailwindcss/tailwind.css";
+import 'tailwindcss/tailwind.css';
+import Link from 'next/link';
 
-import Board from "../components/Board";
-import { useReducer } from "react";
-import { defaultBoard } from "../components/defaultBoard";
+import Board from '../components/Board';
+import { useReducer } from 'react';
+import { defaultBoard } from '../components/defaultBoard';
 
 interface initialState {
   row: number;
@@ -11,10 +12,7 @@ interface initialState {
   gameOver: Boolean;
 }
 
-type ACTIONTYPES =
-  | { type: "onInput"; e: string }
-  | { type: "onDelete" }
-  | { type: "onEnter" };
+type ACTIONTYPES = { type: 'onInput'; e: string } | { type: 'onDelete' } | { type: 'onEnter' };
 
 const initialState = {
   row: 0,
@@ -24,29 +22,33 @@ const initialState = {
 };
 
 const reducer = (state: initialState, action: ACTIONTYPES) => {
-  const { row, letterIndex, board } = state;
+  const { row, letterIndex, board, gameOver } = state;
   const newBoard = [...board];
   switch (action.type) {
-    case "onInput":
+    case 'onInput':
       newBoard[row][letterIndex] = action.e;
       return {
         ...state,
         letterIndex: letterIndex + 1,
         board: newBoard,
       };
-    case "onDelete":
-      newBoard[row][letterIndex - 1] = "";
+    case 'onDelete':
+      newBoard[row][letterIndex - 1] = '';
       return {
         ...state,
         letterIndex: letterIndex - 1,
         board: newBoard,
       };
-    case "onEnter":
-      let currWord = "";
+    case 'onEnter':
+      let currWord = '';
       for (let i = 0; i < 5; i++) {
         currWord += state.board[state.row][i];
       }
-      let isAnswer = currWord === "SHEEP";
+      let isAnswer = currWord === 'SHEEP';
+      if (gameOver)
+        return {
+          ...state,
+        };
       return {
         ...state,
         row: row + 1,
@@ -61,29 +63,49 @@ const wordle = () => {
 
   const onInput = (e: string) => {
     if (state.letterIndex > 4) return;
-    dispatch({ type: "onInput", e });
+    if (state.row > 5) {
+      return;
+    }
+    dispatch({ type: 'onInput', e });
   };
 
   const onDelete = () => {
     if (state.letterIndex === 0) return;
-    dispatch({ type: "onDelete" });
+    dispatch({ type: 'onDelete' });
   };
 
   const onEnter = () => {
     if (state.letterIndex !== 5) return;
-    dispatch({ type: "onEnter" });
+    if (state.gameOver === true) return;
+    dispatch({ type: 'onEnter' });
   };
 
   return (
-    <div className=" bg-slate-200 h-screen w-full flex flex-col items-center justify-center">
-      <p className=" font-serif font-semibold">wordle</p>
-      <Board
-        onInput={onInput}
-        onDelete={onDelete}
-        onEnter={onEnter}
-        currPosition={state}
-      />
-      🐏
+    <div className=' bg-slate-200 h-screen w-full flex flex-col items-center justify-center'>
+      <p className=' font-serif font-semibold'>wordle</p>
+      <Board onInput={onInput} onDelete={onDelete} onEnter={onEnter} currPosition={state} />
+      {state.gameOver ? (
+        <>
+          <span className='mt-2 text-orange-500'>🥳 wow, you're awesome 🦭</span>
+          <Link href='/' className=' text-gray-600'>
+            cd ..
+          </Link>
+        </>
+      ) : (
+        <>
+          <span className='mt-2 '>🐄🦙🐩🐏</span>
+        </>
+      )}
+      {state.row === 6 && !state.gameOver ? (
+        <>
+          <span>😭</span>
+          <Link href='/' className=' text-gray-600'>
+            cd ..
+          </Link>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
