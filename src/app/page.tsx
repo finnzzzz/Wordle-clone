048d7e0e05ae58f7949bf478405e98aca2e4a233
finnzzzz <o8786791@gmail.com>
2023-07-29 18:57:33 +0800

    style: adjust index style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function page() {
   return (
-    <>
-      <h1 className=" text-white text-6xl">
-        <Link href="/wordle">Start</Link>
-      </h1>
-    </>
+    <div className='h-screen flex items-center flex-col justify-center gap-3 bg-slate-200'>
+      <h1 className=' font-semibold text-slate-700 text-3xl'>Wordle</h1>
+      <h2>
+        <Link href='/wordle' className='hover:underline text-2xl '>
+          Start
+        </Link>
+      </h2>
+    </div>
   );
 }
